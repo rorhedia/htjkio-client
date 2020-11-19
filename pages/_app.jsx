6c5a0ac36c65b9a01a7e45ebac65f052e12d4b1c
Daniel Heredia <rorhedia@gmail.com>
2020-11-19 00:45:24 -0600

    Se crea la navbar | Se crea la vista Home

---
+++ pages/_app.jsx
@@ -0,0 +1,8 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "../styles/styles.scss";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
